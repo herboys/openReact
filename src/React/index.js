f8f5f2fb39f265604054f0c3e73750cda04662b5
herboys <42047866+herboys@users.noreply.github.com>
2022-11-02 23:31:14 +0800

    bug
    测试bug

--- src/React/index.js
+++ src/React/index.js
@@ -1,4 +1,48 @@
-const createTextElement = (text) => {
+/**
+ * 创建虚拟 DOM 结构
+ * @param type HTML 元素的类型（h1,p,button）
+ * @param props 对象的属性（{style:{size:10px}} 或 Eventhandlers、classNames 等）
+ * @param children 需要在屏幕上显示的任何东西
+ * @return element 虚拟 DOM
+ */
+function createElement(type, props, ...children) {
+    if (children.length > 0) {
+        console.log(type, props, children, '01')
+        return {
+            type,
+            props: {
+                ...props,
+                children: children.map((child) =>
+                    typeof child === "object" ? child : createTextElement(child)
+                ),
+            },
+        }
+    } else {
+      return   newCreateElement(type)
+    }
+
+
+}
+
+function newCreateElement(data) {
+    const {type, props, ...children} = data
+    console.log(type,'type')
+    console.log(props,'123')
+    return {
+        type,
+        props: {
+            ...props,
+            children: typeof props.children === "object"? props.children.map((child) =>
+                typeof child === "object" ? newCreateElement(child) : createTextElement(child)
+           ):createTextElement(props.children)
+            // children: children.map((child) =>
+            //     typeof child === "object" ? child : createTextElement(child)
+            // ),
+        },
+    }
+}
+
+function createTextElement(text) {
     return {
         type: "TEXT_ELEMENT",
         props: {
@@ -7,29 +51,12 @@ const createTextElement = (text) => {
         },
     };
 }
-/**
- *
- * @param type HTML 元素的类型（h1,p,button）
- * @param props 对象的属性（{style:{size:10px}} 或 Eventhandlers、classNames 等）
- * @param children 需要在屏幕上显示的任何东西
- */
-const createElement = (type, props, ...children) => {
-    return {
-        type,
-        props: {
-            ...props,
-            children: children.map((child) =>
-                typeof child === "object" ? child : createTextElement(child)
-            ),
-        },
-    };
-}
 
 /**
  * @param element 需要在DOM中渲染的元素
  * @param container 在 dom 中渲染的位置
  */
-const render = (element, container) => {
+function render(element, container) {
     // 为元素创造节点
     const dom = element.type == "TEXT_ELEMENT"
         ? document.createTextNode("")
@@ -37,20 +64,25 @@ const render = (element, container) => {
     console.log(dom, element, 'dom')
     // 为 DOM 节点添加属性props
     const isProperty = key => key !== "children"
+    console.log(element,'01')
+    console.log(Object.keys(element.props).filter(isProperty))
     Object.keys(element.props)
         .filter(isProperty)
         .forEach(name => {
-            console.log(name,name.trim() === 'style', '2')
+            console.log(name, 'look')
             if (name.trim() === 'style') {
-                Object.keys(element.props[name]).forEach(v => {
-                    dom[name].setProperty(v, element.props[name][v])
-                })
-                dom[name].setProperty = element.props[name]
-                return
+                Object.keys(element.props[name]).forEach(v =>{
+                        dom[name].setProperty(v, element.props[name][v])
+                    }
+                )
             }
-            dom[name] = element.props[name]
-            console.log(element.props[name], 'look')
-            console.log(dom[name], 'look')
+            console.log(name, 'look')
+            if (name.trim() === 'nodeValue') {
+                console.log(name, 'look')
+                dom[name] = element.props[name]
+                console.log(element.props[name], name, 'look')
+                console.log(dom[name], 'look')
+            }
         })
     console.log(dom, '000')
     // 递归调用
@@ -66,5 +98,4 @@ const React = {
     createElement,
     render,
 }
-
 export default React
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from './React';
 
 const ReactDOM = React
+let backAndTextColor = {
+    backgroundColor:'red',
+    color:'red',
+    fontSize:40
+}
+// /** @jsx React.createElement */
+const dome = (
+    <h1  style={backAndTextColor}>Hi React</h1>
+)
 
-const demo = React.createElement("h1", {style: {color: "red"}}, "Hi React")
-ReactDOM.render(demo, document.getElementById("root"))
+// const element01 = React.createElement("h1", {style: {color: "red"}}, "Hi React")
+const element=React.createElement(dome)
+console.log(element,'999')
+// console.log(React.createElement(dome), element, 'dome')
+ReactDOM.render(element, document.getElementById("root"))
 
 //
 // import React from "react";
-// import ReactDOM from 'react-dom';
+// import {createRoot} from 'react-dom/client';
+//
 // const demo = React.createElement("h1", {style: {color: "red"}}, "Hi React")
-// ReactDOM.render(demo, document.getElementById('root'));
+// const root = createRoot(document.getElementById('root'))
+// console.log(root)
+// root.render(demo);
